881f46ca347d3222c687c1811299fe839d518a97
accrane <austincrane@gmail.com>
2021-10-29 13:32:04 -0400

    add popup

--- inc/scripts.php
+++ inc/scripts.php
@@ -19,6 +19,13 @@ function bellaworks_scripts() {
 		);
 
 	wp_enqueue_script( 
+			'bellaworks-colorbox', 
+			get_template_directory_uri() . '/assets/js/vendors/colorbox.js', 
+			array(), '20120206', 
+			true 
+		);
+
+	wp_enqueue_script( 
 			'bellaworks-custom', 
 			get_template_directory_uri() . '/assets/js/custom.js', 
 			array(), '20120206', 
